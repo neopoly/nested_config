9e5aa82624cabe37c82df27c9f7044fc96133586
Peter Suschlik <ps@neopoly.de>
2012-08-16 18:34:39 +0200

    Refactor with_config tests.

--- test/nested_config_with_config_test.rb
+++ test/nested_config_with_config_test.rb
@@ -4,25 +4,36 @@ class NestedConfigWithConfigTest < NestedConfigSpec
   require 'nested_config/with_config'
   include NestedConfig::WithConfig
 
-  let(:config) do
-    NestedConfig.new.tap do |config|
-      config.nest do |nest|
-        nest.level = 1
-        nest.deep do |deep|
-          deep.level = 2
+  context "with_config" do
+    let(:config) do
+      NestedConfig.new.tap do |config|
+        config.basic = 23
+        config.nest do |nest|
+          nest.level = 1
+          nest.deep do |deep|
+            deep.level = 2
+          end
+        end
+      end
+    end
+
+    test "one key" do
+      refute_change 'config.nest.level' do
+        with_config(config, :nest) do |nest|
+          nest.level = 2
+          assert_equal 2, config.nest.level
         end
       end
     end
   end
 
-  test "with_config with one key" do
-    assert_equal 1, config.nest.level
+  private
 
-    with_config(config, :nest) do |nest|
-      nest.level = 2
-      assert_equal 2, config.nest.level
-    end
-
-    assert_equal 1, config.nest.level
+  def refute_change(code)
+    before = eval(code)
+    yield
+  ensure
+    after = eval(code)
+    assert_equal before, after, "#{code} has changed to #{mu_pp(after)} from #{mu_pp(before)}"
   end
 end
